0b35b75490ea602a54b218bd1775fb6ff52a95f1
nozomi <53336764+zealid@users.noreply.github.com>
2023-08-15 02:56:03 +0000

    React練習

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwitchShowFrag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
   return (
     <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか?</p>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColorfulMessage color="blue">お元気ですか?</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です!</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFrag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(∩´∀｀)∩</p>}
     </>
   );
 };
---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ColorfulMessage = (props) => {
+  const { color, children } = props;
+  const contentStyle = {
+    color,
+    fontSize: "18px",
+  };
+  return <p style={contentStyle}>{children}</p>;
+};
+
+export default ColorfulMessage;
